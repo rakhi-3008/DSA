5b0781c849ab999267bccdb1fba45609ad028d98
Rakhi <rakhipanwar819@gmail.com>
2026-02-02 20:23:05 +0530

    linked list in stl

---
+++ linked_list/list_stl.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+
+using namespace std;
+
+void printList(list<int> ll){
+    list<int>::iterator itr;
+    for(itr=ll.begin(); itr!=ll.end(); itr++){
+        cout<<(*itr)<<"->";
+    }
+    cout<<"NULL"<<endl;
+}
+
+int main(){
+    list<int> ll;
+    ll.push_front(2);
+    ll.push_front(1);
+
+    ll.push_back(3);
+    ll.push_back(4);
+
+    printList(ll);
+
+    cout<<ll.size()<<endl;
+    cout<<"head="<<ll.front()<<endl;
+    cout<<"tail="<<ll.back()<<endl;
+
+    ll.pop_front();
+    printList(ll);
+
+    ll.pop_back();
+    printList(ll);
+
+    return 0;
+}
+
