91f2c34bab1732a488671f59c5bdcf746c68dd19
Rakhi <rakhipanwar819@gmail.com>
2026-03-28 23:35:10 +0530

    find union & intersection

---
+++ hashing/union&intersection.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+void printUnion(vector<int> arr1, vector<int> arr2){
+    unordered_set<int> s;
+
+    for(int el : arr1){
+        s.insert(el);
+    }
+
+    for(int el : arr2){
+        s.insert(el);
+    }
+
+    for(int el : s){
+        cout<<el<<" ";
+    }
+    cout<<endl;
+}
+
+void printIntersection(vector<int> arr1, vector<int> arr2){
+    unordered_set<int> s;
+
+    for(int el : arr1){
+        s.insert(el);
+    }
+
+    for(int el : arr2){
+        if(s.find(el)!=s.end()){    //found
+            cout<<el<<" ";
+            s.erase(el);
+        }
+    }
+    cout<<endl;
+}
+
+
+int main(){
+    vector<int> arr1={7, 3, 9};
+    vector<int> arr2={6, 3, 9, 2, 9, 4};
+
+    cout<<"union: "; 
+    printUnion(arr1, arr2);
+
+    cout<<"intersection: ";
+    printIntersection(arr1, arr2);
+
+    return 0;
+}
