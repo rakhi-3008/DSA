71c9defa233715f7c5989098389ed648a65faa10
Rakhi <rakhipanwar819@gmail.com>
2026-04-04 23:30:17 +0530

    climbing stairs

---
+++ dynamic_programming/climbing_stairs.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int countWaysRec(int n){    //O(2^n)
+    if(n==0 || n==1){
+        return 1;
+    }
+
+    return countWaysRec(n-1)+countWaysRec(n-2);
+}
+
+int countWaysMem(int n, vector<int> &dp){    //O(n)
+    if(n==0 || n==1){
+        return 1;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+
+    dp[n] = countWaysMem(n-1, dp)+countWaysMem(n-2, dp);
+    return dp[n];
+}
+
+int countWaysTab(int n){    //O(n)
+    vector<int> dp(n+1, -1);
+    dp[0]=dp[1]=1;
+
+    for(int i=2; i<=n; i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+
+int main(){
+    int n=4;
+    vector<int> dp(n+1, -1);
+
+    cout<<countWaysRec(n)<<endl;
+    cout<<countWaysMem(n, dp)<<endl;
+    cout<<countWaysTab(n)<<endl;
+
+    return 0;
+}
