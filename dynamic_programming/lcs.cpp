d32e83391ed27ee191dd83417d7175273c0f68f0
Rakhi <rakhipanwar819@gmail.com>
2026-04-16 00:33:38 +0530

    lcs using dp

--- dynamic_programming/lcs.cpp
+++ dynamic_programming/lcs.cpp
@@ -3,7 +3,7 @@
 #include<string>
 using namespace std;
 
-int lscRec(string str1, string str2){
+int lcsRec(string str1, string str2){
     if(str1.size()==0 || str2.size()==0){
         return 0;
     }
@@ -12,19 +12,64 @@ int lscRec(string str1, string str2){
     int m = str2.size();
 
     if(str1[n-1]==str2[m-1]){
-        return 1+ lscRec(str1.substr(0, n-1), str2.substr(0, m-1));
+        return 1+ lcsRec(str1.substr(0, n-1), str2.substr(0, m-1));
     }else{
-      int ans1 =lscRec(str1.substr(0, n-1), str2);
-      int ans2 =lscRec(str1, str2.substr(0, m-1));
+      int ans1 =lcsRec(str1.substr(0, n-1), str2);
+      int ans2 =lcsRec(str1, str2.substr(0, m-1));
       return max(ans1, ans2);
     }
+}
+
+int lcsMem(string str1, string str2, vector<vector<int>> dp){   //O(n*m)
+    if(str1.size()==0 || str2.size()==0){
+        return 0;
+    }
+
+    int n = str1.size();
+    int m = str2.size();
+
+    if(dp[n][m]!=-1){
+        return dp[n][m];
+    }
+
+    if(str1[n-1]==str2[m-1]){
+        dp[n][m]= 1+ lcsMem(str1.substr(0, n-1), str2.substr(0, m-1), dp);
+    }else{
+      int ans1 =lcsMem(str1.substr(0, n-1), str2, dp);
+      int ans2 =lcsMem(str1, str2.substr(0, m-1), dp);
+      dp[n][m] =max(ans1, ans2);
+    }
+    return dp[n][m];
+}
+
+int lcsTab(string str1, string str2){
+    int n = str1.size();
+    int m = str2.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for(int i=1; i<n+1; i++){
+        for(int j=1; j<m+1; j++){
+            if(str1[i-1]==str2[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }else{
+                dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][m];
 }
 
 int main(){
     string str1= "abcdge";
     string str2= "abedg";
 
-    cout<<lscRec(str1, str2)<<endl;
+    int n = str1.size();
+    int m = str2.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+
+    cout<<lcsRec(str1, str2)<<endl;
+    cout<<lcsMem(str1, str2, dp)<<endl;
+    cout<<lcsTab(str1, str2);
 
     return 0;
 }
